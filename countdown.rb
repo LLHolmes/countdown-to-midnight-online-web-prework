3e975af496123fce08bdedc76cf9f70da88a85c2
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-04 15:35:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,18 @@
-#write your code here
-
 def countdown(count)
   x = count
   while x >= 0
     puts "#{count} SECOND(S)!"
     x -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(count)
+  x = count
+  while x >= 0
+    puts "#{count} SECOND(S)!"
+    x -= 1
+    sleep(1)
   end
   "HAPPY NEW YEAR!"
 end
